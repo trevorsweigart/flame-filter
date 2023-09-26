da27e0ed4d6c79fdf43b687efc48b5b98e9bc944
Trevor Sweigart <sweigart.trevor@protonmail.com>
2023-09-26 08:13:11 -0400

    feat: add plotting function

--- HPFilter.py
+++ HPFilter.py
@@ -3,6 +3,14 @@ import numpy as np
 from scipy import ndimage
 from PIL import Image
 
+def plot(data, title):
+    plot.i += 1
+    plt.subplot(3, 2, plot.i)
+    plt.imshow(data, cmap='gray')
+    plt.title(title)
+
+plot.i = 0
+
 # Loading data
 img = Image.open('testImage1.png')
 data = np.array(img, dtype=float)
