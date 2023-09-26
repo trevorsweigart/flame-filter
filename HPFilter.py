0063aec3f40294c70f9e12325b48bb6848e75a81
Trevor Sweigart <sweigart.trevor@protonmail.com>
2023-09-26 08:11:22 -0400

    feat: load test image

--- HPFilter.py
+++ HPFilter.py
@@ -2,3 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import ndimage
 from PIL import Image
+
+# Loading data
+img = Image.open('testImage1.png')
+data = np.array(img, dtype=float)
